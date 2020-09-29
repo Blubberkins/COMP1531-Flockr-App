c65dcde2e267fe86a6d8d6a783b20ee9752e294b
eddieboyemo <eddieboyemo@gmail.com>
2020-09-29 23:01:14 +1000

    fixed some syntax

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,4 +1,5 @@
 import channel
+import channels
 import auth
 import pytest
 from error import InputError
@@ -42,13 +43,13 @@ def test_channel_invite_success():
     channel.channel_invite(login_owner['token'], channel_id, login_user['u_id'])
 
     channel_details = channel.channel_details(login_owner['token'], channel_id)
-    assert channel_details['all_members'] == [{u_id : login_owner['u_id'], name_first : Owner, name_last : Test}, {u_id : login_user['u_id'], name_first : User, name_last : Test}]
+    assert channel_details['all_members'] == [{'u_id' : login_owner['u_id'], 'name_first' : 'Owner', 'name_last' : 'Test'}, {'u_id' : login_user['u_id'], 'name_first' : 'User', 'name_last' : 'Test'}]
 
 # Tests for channel_details
 def test_channel_details_invalid_id():
     auth.auth_register("owner@email.com", "password123", "Owner", "Test")
     login_owner = auth.auth_login("owner@email.com", "password123")
-    channel_id = channels.channels_create(login_owner['token'], "channel", 'u_id')
+    channels.channels_create(login_owner['token'], "channel", 'u_id')
     invalid_channel_id = -1
 
     with pytest.raises(InputError) as e:
@@ -72,8 +73,8 @@ def test_channel_details_success():
 
     channel_details = channel.channel_details(login_owner['token'], channel_id)
     assert channel_details['name'] == "channel"
-    assert channel_details['owner_members'] == [{u_id : login_owner['u_id'], name_first : Owner, name_last : Test}]
-    assert channel_details['all_members'] == [{u_id : login_owner['u_id'], name_first : Owner, name_last : Test}]
+    assert channel_details['owner_members'] == [{'u_id' : login_owner['u_id'], 'name_first' : 'Owner', 'name_last' : 'Test'}]
+    assert channel_details['all_members'] == [{'u_id' : login_owner['u_id'], 'name_first' : 'Owner', 'name_last' : 'Test'}]
 
     auth.auth_register("user@email.com", "password123", "User", "Test")
     login_user = auth.auth_login("user@email.com", "password123")
@@ -81,7 +82,8 @@ def test_channel_details_success():
 
     channel_details = channel.channel_details(login_user['token'], channel_id)
     assert channel_details['name'] == "channel"
-    assert channel_details['owner_members'] == [{u_id : login_owner['u_id'], name_first : Owner, name_last : Test}]
-    assert channel_details['all_members'] == [{u_id : login_owner['u_id'], name_first : Owner, name_last : Test}, {u_id : login_user['u_id'], name_first : User, name_last : Test}]
+    assert channel_details['owner_members'] == [{'u_id' : login_owner['u_id'], 'name_first' : 'Owner', 'name_last' : 'Test'}]
+    assert channel_details['all_members'] == [{'u_id' : login_owner['u_id'], 'name_first' : 'Owner', 'name_last' : 'Test'}, {'u_id' : login_user['u_id'], 'name_first' : 'User', 'name_last' : 'Test'}]
 
-    # Tests for channel_messages
+# Tests for channel_messages
+
