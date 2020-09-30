1f9075a3b9c643be7382666fcd3ca98071fe0845
Emily Tang <z5308868@ad.unsw.edu.au>
2020-09-30 18:57:32 +1000

    Reverted to original auth.py file (made changes on the wrong branch)

--- src/auth.py
+++ src/auth.py
@@ -1,15 +1,8 @@
-import data # need to create a file which has a global variable that can be modified from any of the project files
-
 def auth_login(email, password):
-    for i in len(data["users"]):
-        if email == data["users"][i]["email"] and password == data["users"][i]["password"]:
-            return {
-                'u_id': 1,
-                'token': email,
-            }
-
-        else:
-
+    return {
+        'u_id': 1,
+        'token': '12345',
+    }
 ]
 def auth_logout(token):
     return {
