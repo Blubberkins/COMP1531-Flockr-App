783a4696e79d30769d9eed298efd7861922a0000
Emily Tang <z5308868@ad.unsw.edu.au>
2020-10-01 00:53:35 +1000

    Completed auth_logout function

--- src/auth.py
+++ src/auth.py
@@ -1,11 +1,14 @@
 import data
 from error import InputError
+from error import AccessError
 
 def auth_login(email, password):
-    # Check if email and password are valid or associated with a registered account
-    for i in len(data["users"]):
-        if check(email) == False:
-            raise InputError("Invalid email")
+    # Check if email is valid
+    if check(email) == False:
+        raise InputError("Invalid email")
+    
+    # Check if email and password are associated with a registered account
+    for i in len(data["users"]): 
         elif email != data["users"][i]["email"]:
             raise InputError("No account found")   
         elif password != data["users"][i]["password"]:
@@ -17,9 +20,26 @@ def auth_login(email, password):
             }
 
 def auth_logout(token):
-    return {
-        'is_success': True,
-    }
+    # Check if token matches a registered email
+    for i in len(data["users"]):
+        if token != data["users"][i]["email"]:
+            raise AccessError("Invalid token")
+            return {
+                "is_success": False,
+            }
+
+    # Using check email function to check whether function is email, based on assumption that token is user email
+    if check(token) == False:
+        raise AccessError("Invalid token")
+        return {
+            "is_success": False,
+        }
+    else:
+        # Invalidate token and log the user out
+        token = "invalid"
+        return {
+            "is_success": True,
+        }
 
 def auth_register(email, password, name_first, name_last):
     return {
