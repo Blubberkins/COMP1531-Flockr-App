a020356559b61c8e232a18417ed80e49e15de1c7
Emily Tang <z5308868@ad.unsw.edu.au>
2020-09-30 18:44:58 +1000

    Created global data variable with required data categories, commented specific details for each category (according to spec)

---
+++ src/data.py
@@ -0,0 +1,41 @@
+data = {
+    "user" : {
+        # u_id" : 1, 
+        # email" : "registered@gmail.com", 
+        # "name_first" : "New", 
+        # "name_last" : "User", 
+        # "handle_str" : "newuser",
+    },
+    'users': [
+        {
+            # "u_id" : 1, 
+            # "email" : "registered@gmail.com", 
+            # "name_first" : "New", 
+            # "name_last" : "User", 
+            # "handle_str" : "newuser",
+        }
+    ],
+    "messages" : [
+        {
+            # "message_id" :,
+            # "u_id" :,
+            # "message" :,
+            # "time_created:"
+        }
+    ],
+    "channels" : [
+        {
+            # "channel_id": 1,
+            # "name" : 'channel1',
+        }
+    ],
+    "members" : [
+        {
+            # "u_id" :,
+            # "name_first" :,
+            # "name_last" :,
+
+        }
+
+    ],
+}
