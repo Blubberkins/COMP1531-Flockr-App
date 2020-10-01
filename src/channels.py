f750b729b1f194c27dbfe4c5cd1bf1ac4f40ec20
Aaron Wang <z5308498@ad.unsw.edu.au>
2020-10-02 02:37:46 +1000

    implemented channels_create

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,7 @@
+import data
+import auth
+from error import InputError
+
 def channels_list(token):
     return {
         'channels': [
@@ -19,6 +23,22 @@ def channels_listall(token):
     }
 
 def channels_create(token, name, is_public):
+    
+    # raises InputError if channel name is longer than 20 characters
+    if len(name) > 20:
+        raise InputError("Name is more than 20 characters long.")
+
+    new_channel = {
+        "channel_id": len(data.data['channels']) + 1,
+        "name" : name,
+        "public" : is_public,
+        "owners" : token,
+        "members" : token
+    }
+
+    # add the new channel to the data file
+    data.data['channels'].append(new_channel)
+
     return {
-        'channel_id': 1,
+        'channel_id' : new_channel["channel_id"]
     }
