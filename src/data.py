74758d378c26b93f757bd80a61cadce25fda4437
Jim Huang <z5310201@ad.unsw.edu.au>
2020-10-02 02:05:53 +1000

    Added accounts key, removed User key, fixed white space

--- src/data.py
+++ src/data.py
@@ -1,12 +1,12 @@
 data = {
-    "user" : {
-        # u_id" : 1, 
-        # email" : "registered@gmail.com", 
-        # "name_first" : "New", 
-        # "name_last" : "User", 
-        # "handle_str" : "newuser",
-    },
-    'users': [
+    "accounts": [
+        {
+            # "email" : "registered@gmail.com",
+            # "password" : "validpassword123",
+            # "token" : "registered@gmail.com",
+        }
+    ],
+    "users": [
         {
             # "u_id" : 1, 
             # "email" : "registered@gmail.com", 
@@ -34,8 +34,6 @@ data = {
             # "u_id" :,
             # "name_first" :,
             # "name_last" :,
-
         }
-
     ],
 }
