fb83677090d25d1d499aa7ea25126bea6668131d
Emily Tang <z5308868@ad.unsw.edu.au>
2020-10-04 19:38:39 +1100

    Removed unecessary comments

--- src/channels.py
+++ src/channels.py
@@ -51,46 +51,6 @@ def channels_listall(token):
         all_channels['channels'].append(channel_data)
 
     return all_channels
-    '''
-    u_id = 0
-    # retrieve and store the u_id of the user
-    for x in data['users']:
-
-        if x['token'] == token:
-            u_id = x['u_id']
-            break
-
-    # create the dictionary to be returned by the function
-    all_channels = {
-        "channels" : []
-    }
-
-    # for each existing channel
-    for x in data['channels']:
-        # create a dictionary containing data about the channel
-        channel_data = {"channel_id" : x['channel_id'], "name" : x['name']}
-
-        # if the channel is private
-        if x['public'] == False:
-                is_member = False
-
-                # check if the user is a member of the channel
-                for y in x['all_members']:
-
-                    if y['u_id'] == u_id:
-                        is_member = True
-                        break
-                
-                # if the user is a member add the channel data to all_channels
-                if is_member == True:
-                    all_channels['channels'].append(channel_data)
-
-        # if the channel is public
-        else:
-            all_channels['channels'].append(channel_data)
-    
-    return all_channels
-    '''
 
 def channels_create(token, name, is_public):
     global data
