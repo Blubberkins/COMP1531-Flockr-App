6b2d8b2cc73c93757c423dfb6fc17142a5b5ed69
Emily Tang <z5308868@ad.unsw.edu.au>
2020-10-20 18:53:55 +1100

    Wrote docstring for user_profile

--- src/user.py
+++ src/user.py
@@ -1,4 +1,27 @@
+from error import InputError
+from error import AccessError
+
 def user_profile(token, u_id):
+    """Returns information about a valid user.
+
+    Args:
+        token: A string which acts an authorisation hash.
+        u_id: A unique integer associated with a registered user's account.
+
+    Returns:
+        A dictionary containing information about a user, including there user_id, email, first name, last name and handle.
+    
+    Raises:
+        InputError: When user with u_id is not a valid user.
+    """
+
+    user = {}
+    user["u_id"] = u_id
+    user["email"] = email   
+    user["name_first"] = name_first
+    user["name_last"] = name_last 
+    user["handle_str"] = handle
+  
     return {
         'user': {
         	'u_id': 1,
