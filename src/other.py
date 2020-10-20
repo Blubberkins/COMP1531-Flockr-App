89547cea9e8e4f14fd6bf057902a3d404d77646c
Daniel <danielouyang337@gmail.com>
2020-10-20 14:52:52 +1100

    Moving functions from other_test to other oops

--- src/other.py
+++ src/other.py
@@ -1,4 +1,11 @@
 from data import data
+from error import AccessError, InputError
+
+def clear():
+    global data
+    data["users"].clear()
+    data["channels"].clear()
+    data["members"].clear()
 
 def clear():
     global data
@@ -7,17 +14,55 @@ def clear():
     data["members"].clear()
 
 def users_all(token):
-    return {
-        'users': [
-            {
-                'u_id': 1,
-                'email': 'cs1531@cse.unsw.edu.au',
-                'name_first': 'Hayden',
-                'name_last': 'Jacobs',
-                'handle_str': 'hjacobs',
-            },
-        ],
-    }
+    token_exist = False
+    for user in data['users']:
+        if user['token'] == token:
+            token_exist = True
+            break
+    if token_exist == False:
+        raise AccessError("Token does not exist")
+
+    users_list = []
+    for user in data['users']:
+        user_dict = {
+            "u_id": user['u_id'],
+            "email": user['email'],
+            "name_first": user['name_first'],
+            "name_last": user['name_last'],
+            "handle_str": user['handle_str']
+        }
+        users_list.append(user_dict)
+
+    return users_list
+
+def admin_userpermission_change(token, u_id, permission_id):
+    global data
+
+    token_exist = False
+    for user in data['users']:
+        if user['token'] == token:
+            token_exist = True
+            break
+    if token_exist == False:
+        raise AccessError("Token does not exist")
+    
+    token_true = False
+    if user['permission_id'] == 1:
+        token_true = True
+    if token_true == False:
+        raise AccessError("Authorised user does not have permission")
+
+    for member in data['users']:
+        if member['u_id'] == u_id:
+            u_id_true = True
+            break
+    if u_id_true == False:
+        raise InputError("User does not exist")
+
+    if permission_id != 1 and permission_id != 2:
+        raise InputError("Permission id does not exist")
+
+    member['permission_id'] = permission_id
 
 def search(token, query_str):
     return {
