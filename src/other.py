c0062410efaeff09877b1fd19e98eaa669929bb7
Aaron Wang <z5308498@ad.unsw.edu.au>
2020-10-20 20:37:30 +1100

    implemented search function

--- src/other.py
+++ src/other.py
@@ -78,13 +78,66 @@ def admin_userpermission_change(token, u_id, permission_id):
     member['permission_id'] = permission_id
 
 def search(token, query_str):
-    return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-    }
+    """
+    Given a query string, return a collection of messages in all of the channels that the user has joined that match the query
+        Args:
+            token: String which is used as an authorisation hash
+            query_str: String that is being searched for
+    """
+
+    # checking for valid token
+    token_exist = False
+    for user in data['users']:
+
+        if user['token'] == token:
+            token_exist = True
+            break
+
+    if token_exist == False:
+        raise AccessError("Token does not exist")
+
+    # retrieve and store the u_id of the user
+    u_id = 0
+    for x in data['users']:
+
+        if x['token'] == token:
+            u_id = x['u_id']
+            break
+
+    # define a list of channel_ids from the channels the user has joined
+    joined_channel_ids = []
+
+    # x loops through each channel
+    for x in data['channels']:
+
+        # y loops through each member in all_members
+        for y in x['all_members']:
+
+            # if the user is a member of the channel, add the channel id to joined_channel_ids and loop to the next channel
+            if y['u_id'] == u_id:
+                joined_channel_ids.append(x['channel_id'])
+                break
+
+    # define a list of dictionaries containing the search results that will be returned by the function
+    search_results = {'messages' : []}
+
+    # x loops through each message
+    for x in data['messages']:
+
+        # if the search string appears in the message
+        if query_str in x['message']:
+
+            # y loops through the user's joined channel ids
+            for y in joined_channel_ids:
+
+                # if the message was sent in a channel the user has joined, add the message info to search_results and loop to the next message
+                if x['channel_id'] == y:
+                    search_results['messages'].append({
+                        'message_id': x['message_id'],
+                        'u_id': x['u_id'],
+                        'message': x['message'],
+                        'time_created': x['time_created'],
+                    })
+                    break
+
+    return search_results
