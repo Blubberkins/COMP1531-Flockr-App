60b57a529fa965e1bf6b902b2df654c70c7f5a90
Aaron Wang <z5308498@ad.unsw.edu.au>
2020-10-20 16:58:09 +1100

    updated to match data branch

--- src/data.py
+++ src/data.py
@@ -1,32 +1,23 @@
 data = {
-    "accounts": [
-        {
-            # "email" : "registered@gmail.com",
-            # "password" : "validpassword123",
-            # "token" : "registered@gmail.com",
-        }
-    ],
     "users": [
-        {
+        #{
             # "u_id" : 1, 
             # "email" : "registered@gmail.com", 
+            # "password" : "validpassword123",
+            # "token" : "registered@gmail.com",
             # "name_first" : "New", 
             # "name_last" : "User", 
             # "handle_str" : "newuser",
-        }
-    ],
-    "messages" : [
-        {
-            # "message_id" :,
-            # "u_id" :,
-            # "message" :,
-            # "time_created:"
-        }
+            # "permission_id" : 1,
+        #}
     ],
     "channels" : [
         {
             # "channel_id": 1,
             # "name" : 'channel1',
+            # "is_public" : True
+            # "owner_members" : [{u_id : , name_first : , name_last : },...]
+            # "all_members" : [{u_id : , name_first : , name_last : },...]
         }
     ],
     "members" : [
@@ -36,4 +27,14 @@ data = {
             # "name_last" :,
         }
     ],
+    "messages": [
+        {
+            # "message_id":,
+            # "u_id":,
+            # "message":,
+            # "time_created":,
+            # "channel_id":,
+        }    
+    ],
+    "num_messages": 0
 }
