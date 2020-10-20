9d5c84c83b0345a36d2a421331e6da46eaa46e3e
eddieboyemo <eddieboyemo@gmail.com>
2020-10-20 19:15:14 +1100

    finished blackbox testing for message_test

--- src/message_test.py
+++ src/message_test.py
@@ -44,7 +44,7 @@ def test_message_send_success():
 def test_message_remove_no_messages():
     clear()
     login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
-    channel_id = channels.channels_create(login_owner['token'], "channel", True)
+    channels.channels_create(login_owner['token'], "channel", True)
 
     with pytest.raises(InputError):
         message.message_remove(login_owner['token'], 1)
@@ -81,3 +81,48 @@ def test_message_admin_remove_success():
     message.message_send(login_user["token"], channel_id["channel_id"], "sample message")
 
     assert message.message_remove(login_owner["token"], 0) == {}
+
+# Tests for message_edit
+def test_message_edit_1000_characters():
+    clear()
+    login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
+    channel_id = channels.channels_create(login_owner['token'], "channel", True)
+
+    message.message_send(login_owner["token"], channel_id["channel_id"], "sample message")
+
+    with pytest.raises(InputError):
+        message.message_edit(login_owner["token"], 0, "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce")
+        message.message_edit(login_owner["token"], 0, "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores e")
+
+def test_message_edit_not_message_sender():
+    clear()
+    login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
+    channel_id = channels.channels_create(login_owner['token'], "channel", True)
+    login_user = auth.auth_register("user@email.com", "password123", "User", "Test")
+    channel.channel_join(login_user["token"], channel_id["channel_id"])
+
+    message.message_send(login_owner["token"], channel_id["channel_id"], "sample message")
+
+    with pytest.raises(AccessError):
+        message.message_edit(login_user["token"], 0, "edited message")
+
+def test_message_owner_success():
+    clear()
+    login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
+    channel_id = channels.channels_create(login_owner['token'], "channel", True)
+    login_user = auth.auth_register("user@email.com", "password123", "User", "Test")
+    channel.channel_join(login_user["token"], channel_id["channel_id"])
+
+    message.message_send(login_user["token"], channel_id["channel_id"], "sample message") 
+
+    assert message.message_edit(login_owner["token"], 0, "edited message") == {}
+
+def test_message_edit_empty_edit():
+    clear()
+    login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
+    channel_id = channels.channels_create(login_owner['token'], "channel", True)
+
+    message.message_send(login_owner["token"], channel_id["channel_id"], "sample message") 
+    assert message.message_edit(login_owner["token"], 0, "") == {}
+    
+
