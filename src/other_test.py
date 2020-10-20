d34a1670f1f141f012612a97b0d95404c691a75c
Daniel Ouyang <d.ouyang.1@student.unsw.edu.au>
2020-10-20 17:15:11 +1100

    Added docstrings to test_user_all and test_changepermission

--- src/other_test.py
+++ src/other_test.py
@@ -10,12 +10,14 @@ from error import AccessError
 
 # test functions for users_all
 def test_users_all_invalid_token():
+    """tests for invalid token"""
     other.clear()
     with pytest.raises(AccessError):
         other.users_all("invalid_token")
         other.users_all("")
 
 def test_users_all_successful():
+    """tests for user_all sucess"""
     other.clear()
     first_user = auth.auth_register("user1@email.com", "password123", "First", "User")
     assert other.users_all(first_user['token']) == [{'u_id' : first_user['u_id'], 'email' : "user1@email.com", 'name_first' : "First", 'name_last' : "User", 'handle_str' : "firstuser"}]
@@ -26,6 +28,7 @@ def test_users_all_successful():
 
 # test functions for admin_userpermission_change
 def test_admin_userpermission_change_invalid_id():
+    """tests for invalid ids"""
     other.clear()
     login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
     login_user = auth.auth_register("user@email.com", "password321", "User", "Test")
@@ -38,6 +41,7 @@ def test_admin_userpermission_change_invalid_id():
         other.admin_userpermission_change(login_owner['token'], invalid_u_id, invalid_permission_id)
 
 def test_admin_userpermission_change_invalid_token():
+    """tests for invalid token"""
     other.clear()
     login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
     login_user = auth.auth_register("user@email.com", "password321", "User", "Test")
@@ -47,6 +51,7 @@ def test_admin_userpermission_change_invalid_token():
         other.admin_userpermission_change(login_user['token'], login_owner['u_id'], 2)
 
 def test_admin_userpermission_change_success():
+    """tests for change permission sucess"""
     other.clear()
     login_owner = auth.auth_register("owner@email.com", "password123", "Owner", "Test")
     login_user = auth.auth_register("user@email.com", "password321", "User", "Test")
