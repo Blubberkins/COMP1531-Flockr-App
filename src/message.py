9fc61470552b2f0d041108f269678948596eb35a
eddieboyemo <eddieboyemo@gmail.com>
2020-10-20 20:46:15 +1100

    finished prototype for message_edit

--- src/message.py
+++ src/message.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timezone
 from data import data
-import channel
-import channels
 from error import AccessError, InputError
 
 def message_send(token, channel_id, message):
@@ -11,14 +9,14 @@ def message_send(token, channel_id, message):
     channel_index = 0
     for channels in data["channels"]:
         if channels["channel_id"] == channel_id:
-            valid_channel_id == True
+            valid_channel_id = True
             channel_index = channels
             break
     if not valid_channel_id:
         raise InputError("Invalid channel")
     if len(message) > 1000:
         raise InputError("Message is larger than 1000 characters")
-    
+
     # Check AccessError
     valid_token = False
     u_id = -1
@@ -61,7 +59,7 @@ def message_remove(token, message_id):
             break
     if not does_message_exist:
         raise InputError("Message has already been deleted")
-    
+
     # Check AccessError
     u_id = -1
     u_id_permission = -1
@@ -83,5 +81,34 @@ def message_remove(token, message_id):
     }
 
 def message_edit(token, message_id, message):
+    # Check message length
+    if len(message) > 1000:
+        raise InputError("Message is larger than 1000 characters")
+
+    # Check AccessError
+    u_id = -1
+    u_id_permission = -1
+    for users in data["users"]:
+        if users["token"] == token:
+            u_id = users["u_id"]
+            users["permission_id"] = u_id_permission
+            break
+    message_owner_or_flock_owner = False
+    message_index = -1
+    for messages in data["messages"]:
+        if messages["message_id"] == message_id:
+            message_index = messages
+            break
+    if data["messages"][message_index]["u_id"] == u_id:
+        message_owner_or_flock_owner = True
+    if u_id_permission == 1:
+        message_owner_or_flock_owner = True
+    if not message_owner_or_flock_owner:
+        raise AccessError("User is not a flock owner or the original user who sent the message")
+
+    if message == "":
+        return message_remove(token, message_id)
+    data["messages"][message_index]["message"] = message
     return {
     }
+    
