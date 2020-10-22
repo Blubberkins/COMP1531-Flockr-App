513e12709983f861e8e4a576fe59ccb572a1f264
Emily Tang <z5308868@ad.unsw.edu.au>
2020-10-22 22:25:45 +1100

    Set up http_user_test.py with function names

---
+++ src/http_user_test.py
@@ -0,0 +1,13 @@
+import user
+
+# get
+def test_http_user_profile():
+
+# put
+def test_http_user_profile_setname():
+
+# put
+def test_http_user_profile_setemail():
+
+# put
+def test_http_user_profile_sethandle():
