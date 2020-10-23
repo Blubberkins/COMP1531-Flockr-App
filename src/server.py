5bcb3475b7d10d03b434218b315f773c7fd2c814
Daniel <danielouyang337@gmail.com>
2020-10-23 19:33:41 +1100

    Wrapped channel_invite, channel_details, channel_addowner and channel_removeowner

--- src/server.py
+++ src/server.py
@@ -3,6 +3,7 @@ from json import dumps
 from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
+import channel
 
 def defaultHandler(err):
     response = err.get_response()
@@ -22,7 +23,7 @@ APP.config['TRAP_HTTP_EXCEPTIONS'] = True
 APP.register_error_handler(Exception, defaultHandler)
 
 # Example
-@APP.route("/echo", methods=['GET'])
+@APP.route("/echo", methods=['GET']) 
 def echo():
     data = request.args.get('data')
     if data == 'echo':
@@ -30,6 +31,30 @@ def echo():
     return dumps({
         'data': data
     })
+
+@app.route('/channel/invite', methods=['POST'])
+def http_channel_invite():
+    data = request.get_json()
+    response = channel.channel_invite(data['token'], data['channel_id'], data['u_id'])
+    return dumps(response)
+
+@app.route('/channel/details', methods=['GET'])
+def http_channel_details():
+    data = request.get_json()
+    response = channel.channel_details(data['token'], data['channel_id'])
+    return dumps(response)
+
+@app.route('/channel/addowner', methods=['POST'])
+def http_channel_invite():
+    data = request.get_json()
+    response = channel.channel_addowner(data['token'], data['channel_id'], data['u_id'])
+    return dumps(response)
+
+@app.route('/channel/removeowner', methods=['POST'])
+def http_channel_invite():
+    data = request.get_json()
+    response = channel.channel_removeowner(data['token'], data['channel_id'], data['u_id'])
+    return dumps(response)
 
 if __name__ == "__main__":
     APP.run(port=0) # Do not edit this port
