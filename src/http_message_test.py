952a71ff08be45b8b10c12c7954d9442e99ec95e
eddieboyemo <eddieboyemo@gmail.com>
2020-10-24 19:14:49 +1100

    made some edits to tests for message_send

--- src/http_message_test.py
+++ src/http_message_test.py
@@ -98,3 +98,17 @@ def create_unique_channel(user, name, is_public):
     r = requests.post(f"{url}/channels/create", json=channel)
     return r.json()
 
+# Tests for message_send
+def test_http_message_send_input_error(url):
+    clear()
+    login_owner = reg_owner(url)
+    channel_id = create_unique_channel(login_owner, "channel", True)
+    
+    large_string = {
+        "token": login_owner["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce"
+    }
+
+    r = requests.post(f"{url}/message/send", json=large_string)
+    payload = r.json()
