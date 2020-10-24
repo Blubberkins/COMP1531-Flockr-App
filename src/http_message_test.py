ccb4a4ef0f6b973f83d74f678327aab6c45953ed
eddieboyemo <eddieboyemo@gmail.com>
2020-10-24 20:11:31 +1100

    finished http tests for message send

--- src/http_message_test.py
+++ src/http_message_test.py
@@ -104,11 +104,111 @@ def test_http_message_send_input_error(url):
     login_owner = reg_owner(url)
     channel_id = create_unique_channel(login_owner, "channel", True)
     
-    large_string = {
+    large_string_1 = {
         "token": login_owner["token"],
         "channel_id": channel_id["channel_id"],
         "message": "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce"
     }
 
-    r = requests.post(f"{url}/message/send", json=large_string)
+    r = requests.post(f"{url}/message/send", json=large_string_1)
     payload = r.json()
+    assert payload["message"] == "Message is larger than 1000 characters"
+    assert payload["code"] == 400
+
+    large_string_2 = {
+        "token": login_owner["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores e"
+    }
+
+    r = requests.post(f"{url}/message/send", json=large_string_2)
+    payload = r.json()
+    assert payload["message"] == "Message is larger than 1000 characters"
+    assert payload["code"] == 400
+
+    large_string_3 = {
+        "token": login_owner["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li grammatica, li pronunciation e li plu commun vocabules. Omnicos directe al desirabilite de un nov lingua franca: On refusa continuar payar custosi traductores. At solmen va esser necessi far uniform grammatica, pronunciation e plu sommun paroles. Ma quande lingues coalesce, li grammatica del resultant lingue es plu simplic e regulari quam ti del coalescent lingues. Li nov lingua franca va esser plu simplic e regulari quam li existent Europan lingues. It va esser tam simplic quam Occidental in fact, it va esser Occidental. A un Angleso it va semblar un simplificat Angles, quam un skeptic Cambridge amico dit me que Occidental es. Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular. Li lingues differe solmen in li g"
+    }
+
+    r = requests.post(f"{url}/message/send", json=large_string_3)
+    payload = r.json()
+    assert payload["message"] == "Message is larger than 1000 characters"
+    assert payload["code"] == 400
+
+    non_existent_channel_id = {
+        "token": login_owner["token"],
+        "channel_id": -1,
+        "message": "sample message"
+    }
+
+    r = requests.post(f"{url}/message/send", json=non_existent_channel_id)
+    payload = r.json()
+    assert payload["message"] == "Invalid channel"
+    assert payload["code"] == 400
+
+def test_http_message_send_access_error(url):
+    clear()
+    login_owner = reg_owner(url)
+    channel_id = create_unique_channel(login_owner, "channel", True)
+    login_user = reg_user(url)
+
+    invalid_user = {
+        "token": login_user["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "sample message"
+    }
+
+    r = requests.post(f"{url}/message/send", json=invalid_user)
+    payload = r.json()
+    assert payload["message"] == "The user has not joined the channel they are trying to post to"
+    assert payload["code"] == 400
+
+def test_http_test_message_send_success(url):
+    clear()
+    login_owner = reg_owner(url)
+    channel_id = create_unique_channel(login_owner, "channel", True)
+    login_user = reg_user(url)
+    inv_user(url, login_owner, login_user, channel_id["channel_id"])
+
+    message_1 = {
+        "token": login_owner["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "sample message"
+    }
+
+    r = requests.post(f"{url}/mesage/send", json=message_1)
+    payload = r.json()
+    assert payload["message_id"] == 0
+
+    message_2 = {
+        "token": login_user["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "sample message"
+    }
+
+    r = requests.post(f"{url}/mesage/send", json=message_2)
+    payload = r.json()
+    assert payload["message_id"] == 1
+
+    message_3 = {
+        "token": login_owner["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores."
+    }
+
+    r = requests.post(f"{url}/mesage/send", json=message_3)
+    payload = r.json()
+    assert payload["message_id"] == 2
+
+    message_4 = {
+        "token": login_user["token"],
+        "channel_id": channel_id["channel_id"],
+        "message": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores."
+    }
+
+    r = requests.post(f"{url}/mesage/send", json=message_4)
+    payload = r.json()
+    assert payload["message_id"] == 3
+
